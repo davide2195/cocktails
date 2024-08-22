e974ca38579b0834d20de0a6bd2346bc8ec0a566
Juan David <j21d@outlook.com>
2024-08-23 01:13:32 +0200

    docs

---
+++ docs/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-AQZ6WUOM.mjs";import"./chunk-T32CHBCB.mjs";import"./chunk-EGKF6ILF.mjs";import"./chunk-NGHQJVXR.mjs";import"./chunk-SMSHTEMW.mjs";import"./chunk-LESQBET5.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
