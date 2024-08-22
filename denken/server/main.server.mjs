9a106b1948d7046930a0ed33a01654d9c8479721
Juan David <j21d@outlook.com>
2024-08-23 00:02:10 +0200

    docs

--- denken/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-AQZ6WUOM.mjs";import"./chunk-T32CHBCB.mjs";import"./chunk-EGKF6ILF.mjs";import"./chunk-NGHQJVXR.mjs";import"./chunk-SMSHTEMW.mjs";import"./chunk-LESQBET5.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
